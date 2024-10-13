10189e07b6d4a797017446a5b92d38c213e91770
Abubaker Naji <abubaker.fg@gmail.com>
2024-10-13 14:41:17 +0200

    fixed it for you query params

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -2,7 +2,6 @@ const { Model } = require("mongoose");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
-
 exports.deleteOne = (Model, relations = []) =>
   catchAsync(async (req, res, next) => {
     // Find the document
@@ -19,10 +18,9 @@ exports.deleteOne = (Model, relations = []) =>
       }
     }
 
-  
     if (hasRelations) {
       doc.deletedAt = Date.now();
-      await doc.save(); 
+      await doc.save();
     } else {
       await Model.findByIdAndDelete(req.params.id);
     }
@@ -45,7 +43,6 @@ exports.updateOne = (Model) =>
       updates.images = req.files.map((file) => file.path);
     }
     updates.updatedAt = Date.now();
-    
 
     const doc = await Model.findByIdAndUpdate(req.params.id, updates, {
       new: true,
@@ -66,7 +63,7 @@ exports.updateOne = (Model) =>
 exports.createOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const newModelData = { ...req.body };
-   
+
     if (req.file) {
       newModelData.image = req.file.path;
     } else if (req.files) {
@@ -99,7 +96,10 @@ exports.getAll = (Model) =>
 
 exports.getOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const getModel = await Model.findById({ _id: req.params.id, deletedAt: null });
+    const getModel = await Model.findById({
+      _id: req.params.id,
+      deletedAt: null,
+    });
     res.status(200).json({
       status: "success",
       message: "Got the Selected Entitie Successfully",
@@ -108,42 +108,95 @@ exports.getOne = (Model) =>
       },
     });
   });
-  exports.getWithFilter = (Model, populateOptions = []) =>
-    catchAsync(async (req, res, next) => {
-      const filter = { ...req.filter, deletedAt: null };
-      //console.log(filter);
-      let query = Model.find(filter);
-  
-      if (req.body.sort) {
-        query = query.sort({ createdAt: -1 });
-      }
-      if (req.body.requestLimit) {
-        query = query.limit(req.body.requestLimit);
-      }
-     
-      populateOptions.forEach((option) => {
-        query = query.populate(option);
-      });
-  
-      const getModels = await query;
-  
-      res.status(200).json({
-        status: "success",
-        message: "Got the Selected Entities Successfully",
-        data: {
-          data: getModels,
-        },
-      });
+exports.getWithFilter = (Model, populateOptions = []) =>
+  catchAsync(async (req, res, next) => {
+    const filter = { ...req.filter, deletedAt: null };
+
+    //console.log(filter);
+    let query = Model.find(filter);
+
+    if (req.query.sort) {
+      query = query.sort({ createdAt: -1 });
+    }
+    if (req.query.requestLimit) {
+      query = query.limit(req.query.requestLimit);
+    }
+
+    populateOptions.forEach((option) => {
+      query = query.populate(option);
     });
-  
+
+    const getModels = await query;
+
+    res.status(200).json({
+      status: "success",
+      message: "Got the Selected Entities Successfully",
+      data: {
+        data: getModels,
+      },
+    });
+  });
 
 exports.setFilter = catchAsync(async (req, res, next) => {
-  let { requestLimit, sort, ...filter } = req.body || {};
- // let filter = req.body || {};
+  //console.log(req.query);
+
+  let {
+    requestLimit,
+    sort,
+    createdAtFrom,
+    createdAtTo,
+    coordinates,
+    maxDistance,
+    priceFrom,
+    priceTo,
+    ...filter
+  } = req.query || {};
+  
+
+  if (coordinates && maxDistance) {
+    const coordinates = req.query.coordinates.split(",").map(Number);
+    const maxDistance = Number(req.query.maxDistance);
+
+    filter.location = {
+      $near: {
+        $geometry: {
+          type: "Point",
+          coordinates: coordinates,
+        },
+        $maxDistance: maxDistance,
+      },
+    };
+  }
+
+  if (priceFrom || priceTo) {
+    filter.price = {};
+    if (priceFrom) {
+      filter.price["$gte"] = Number(priceFrom);
+    }
+    if (priceTo) {
+      filter.price["$lte"] = Number(priceTo);
+    }
+  }
+  if (filter.name) {
+    filter.name = {
+      $regex: filter.name,
+      $options: "i",
+    };
+  }
+
+  if (createdAtFrom || createdAtTo) {
+    filter.createdAt = {};
+    if (createdAtFrom) {
+      filter.createdAt["$gte"] = new Date(createdAtFrom);
+    }
+    if (createdAtTo) {
+      filter.createdAt["$lte"] = new Date(createdAtTo);
+    }
+  }
+
   req.filter = filter;
   next();
 });
-
 
 exports.addlocations = catchAsync(async (req, res, next) => {
   let location = null;
